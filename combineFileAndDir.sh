7fe7e109ed9eb389dd604ce5eff99b5f168c79dd
nonvioce <35244429+nonvioce@users.noreply.github.com>
2022-02-28 11:28:26 +0800

    Update combineFileAndDir.sh

--- combineFileAndDir.sh
+++ combineFileAndDir.sh
@@ -1,9 +1,16 @@
 #/bin/bash
 #在同一目录下，批量将文件放入同名文件夹
 #输入为路径
-Folder_A=$1 
-for file_a in ${Folder_A}/*
+Folder=$1
+if [ "${Folder:-1}" != '/' ];then
+    Folder=$Folder'/'
+    echo "已将$1格式化为$Folder"
+fi
+for file in ${Folder}*
 do  
-    echo $file_a
-    mv $file_a ./${file_a%.*}/  
+    if [ -f $file ];then
+    	echo "正在移动$file"
+    	(mv $file ${file%.*}/)||(mkdir ${file%.*}/;echo "已创建目录${file%.*}/";mv $file ${file%.*}/)
+    fi
 done
+echo "批量移动完成"
